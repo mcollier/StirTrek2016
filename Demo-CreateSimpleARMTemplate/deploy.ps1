874fb6311d76a6769fd1b02937253d511b26edb7
Michael S. Collier <buckeye01@hotmail.com>
2016-05-06 15:20:09 -0400

    Minor formatting and path updates.

--- Demo-CreateSimpleARMTemplate/deploy.ps1
+++ Demo-CreateSimpleARMTemplate/deploy.ps1
@@ -1,16 +1,20 @@
 ﻿#Login-AzureRmAccount
 
-Select-AzureRmSubscription -SubscriptionId ""
+#Select-AzureRmSubscription -SubscriptionId ""
 
 $location = "northcentralus"
 $resourceGroupName = "stirtrek2016-webapp"
 
-$templateFile = "C:\Users\mcollier\Dropbox\Presentations\Work on Your ARM Strength\StirTrek 2016\Demo-CreateSimpleARMTemplate\azuredeploy.json"
-$templateParamFile = "C:\Users\mcollier\Dropbox\Presentations\Work on Your ARM Strength\StirTrek 2016\Demo-CreateSimpleARMTemplate\azuredeploy.parameters.json"
+$templateFile = "C:\Projects\StirTrek2016\Demo-CreateSimpleARMTemplate\azuredeploy.json"
+$templateParamFile = "C:\Projects\StirTrek2016\Demo-CreateSimpleARMTemplate\azuredeploy.parameters.json"
 
 New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
 
-Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFile -TemplateParameterFile $templateParamFile -Mode Incremental -Verbose -Debug
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+                                    -TemplateFile $templateFile `
+                                    -TemplateParameterFile $templateParamFile `
+                                    -Mode Incremental `
+                                    -Verbose -Debug
 
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName `
                                    -TemplateFile $templateFile `
